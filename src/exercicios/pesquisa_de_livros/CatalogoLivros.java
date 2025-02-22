7dfb8f85e8fa112d4a497add44e8cff0ecf9d2b8
Math012 <math111.0811@gmail.com>
2025-02-22 20:15:21 -0300

    Resolvendo o exercicio com List 3

---
+++ src/exercicios/pesquisa_de_livros/CatalogoLivros.java
@@ -0,0 +1,37 @@
+package exercicios.pesquisa_de_livros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    public static void main(String[] args) {
+        List<Livro> livros = new ArrayList<>();
+        Livro livroMethods = new Livro();
+
+        livros.add(new Livro("HTML E CSS","Adams",2001));
+        livros.add(new Livro("Desenvolvimento Android","Kyle",2007));
+        livros.add(new Livro("Como desenhar","Josh",2003));
+        livros.add(new Livro("Aprendendo c++","Vincent",2008));
+        livros.add(new Livro("Segredo do C","Lee",2010));
+        livros.add(new Livro("Princípios do Cobol","Giuseppe",1987));
+        livros.add(new Livro("Cyber Segurança","Caire",1996));
+        livros.add(new Livro("Segurança em TI","Care",1999));
+        livros.add(new Livro("Interface de usuário","Lisa",1994));
+        livros.add(new Livro("Internet das coisas","Giuseppe",1985));
+        livros.add(new Livro("Tudo sobre a internet","Becker",2011));
+        livros.add(new Livro("Aprendendo Spring Boot","Giuseppe",2024));
+
+        System.out.println("======= pesquisando por autor [Giuseppe] =======");
+        livroMethods.pesquisarPorAutor(livros,"Giuseppe");
+        System.out.println("--------------------------------------------------");
+        System.out.println("======= pesquisando por ano [2000 - 2010] =======");
+        livroMethods.pesquisarPorIntervaloAnos(livros,2000,2010);
+        System.out.println("--------------------------------------------------");
+        System.out.println("======= pesquisando por ano [1980 - 1999] =======");
+        livroMethods.pesquisarPorIntervaloAnos(livros,1980,1999);
+        System.out.println("--------------------------------------------------");
+        System.out.println("======= pesquisando por titulo [Aprendendo Spring Boot] =======");
+        livroMethods.pesquisarPorTitulo(livros, "Aprendendo Spring Boot");
+
+    }
+}
---
+++ src/exercicios/pesquisa_de_livros/Livro.java
@@ -0,0 +1,91 @@
+package exercicios.pesquisa_de_livros;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private int ano;
+
+    public Livro(){}
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void adicionarLivro(List<Livro> livros, String titulo, String autor, int ano){
+        livros.add(new Livro(titulo,autor,ano));
+    }
+
+    public void pesquisarPorAutor(List<Livro> livros, String autor){
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getAutor() == autor){
+                System.out.println(
+                        "Titulo: "
+                        + livros.get(i).getTitulo()
+                        + ", Autor: "
+                        + livros.get(i).getAutor()
+                        + ", Ano: " + livros.get(i).getAno()
+                );
+            }
+        }
+    }
+
+    public void pesquisarPorIntervaloAnos(List<Livro> livros, int anoInicial, int anoFinal){
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getAno() >= anoInicial && livros.get(i).getAno() <= anoFinal){
+                System.out.println(
+                        "Titulo: "
+                                + livros.get(i).getTitulo()
+                                + ", Autor: "
+                                + livros.get(i).getAutor()
+                                + ", Ano: " + livros.get(i).getAno()
+                );
+            }
+        }
+    }
+
+    public void pesquisarPorTitulo(List<Livro> livros, String titulo){
+        for (int i = 0; i < livros.size(); i++) {
+            if (livros.get(i).getTitulo() == titulo){
+                System.out.println(
+                        "Titulo: "
+                                + livros.get(i).getTitulo()
+                                + ", Autor: "
+                                + livros.get(i).getAutor()
+                                + ", Ano: " + livros.get(i).getAno()
+                );
+            }
+        }
+    }
+}
+
+
